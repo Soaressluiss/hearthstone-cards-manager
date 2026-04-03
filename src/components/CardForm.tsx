87d9a31666246bca38ceee370c553d7eef1c55a7
Luis Soares <luissoaress.dev@gmail.com>
2026-04-03 15:29:20 -0300

    feat: refactor card addition form into a modal and adjust UI

--- src/components/CardForm.tsx
+++ src/components/CardForm.tsx
@@ -3,8 +3,18 @@ import { useForm } from "react-hook-form";
 import { cardSchema, type CardFormData } from "../schemas/CardSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CardClass, CardType } from "../types/Card";
+import { Dialog, DialogClose, DialogContent, DialogTitle } from "./ui/dialog";
+import { useRef } from "react";
 
-export default function CardForm() {
+interface CardFormProps {
+  openModal: boolean;
+  handleOpenModal: (isOpen: boolean) => void;
+}
+
+export default function CardForm({
+  openModal,
+  handleOpenModal,
+}: CardFormProps) {
   const {
     register,
     handleSubmit,
@@ -15,129 +25,157 @@ export default function CardForm() {
     resolver: zodResolver(cardSchema),
     mode: "onChange",
   });
+  const currentId = useRef(0);
 
   const tipo = watch("tipo");
   const classe = watch("classe");
 
+  function generateId() {
+    currentId.current += 1;
+    return currentId.current;
+  }
   const onSubmit = async (data: CardFormData) => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
     const newCard = {
-      id: Date.now(),
+      id: generateId(),
+      mana: Math.floor(Math.random() * 11),
       ...data,
     };
     console.log(newCard);
     reset();
+    handleOpenModal(false);
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="bg-surface flex w-1/2 flex-col gap-2 rounded-xl border border-white/5 p-5 pb-8 shadow-md"
-    >
-      <h2 className="font-belwe text-primary text-center text-3xl">
-        Cadastrar carta
-      </h2>
-      <div className="flex flex-col gap-2">
-        <label
-          htmlFor="nome"
-          className="text-muted font-sans text-sm tracking-wider"
+    <Dialog open={openModal} onOpenChange={handleOpenModal}>
+      <DialogContent aria-describedby={undefined}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="bg-surface border-primary-soft/40 flex w-full flex-col gap-2 rounded-xl border p-5 pb-8"
         >
-          Nome
-        </label>
-        <input
-          id="nome"
-          {...register("nome")}
-          placeholder="Nome da carta"
-          className="bg-background focus:border-primary rounded-lg border border-white/10 px-3 py-3 font-sans text-base outline-none"
-        />
-        <p className="text-xs text-red-400">{errors.nome?.message}</p>
-      </div>
+          <DialogTitle className="font-belwe text-primary text-center text-3xl">
+            Adicionar carta
+          </DialogTitle>
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="nome"
+              className="text-muted font-belwe text-sm tracking-wider"
+            >
+              Nome da Carta
+            </label>
+            <input
+              id="nome"
+              {...register("nome")}
+              placeholder="Guardião da Floresta"
+              className="bg-background/40 focus:border-primary font-belwe rounded-lg border border-white/10 px-3 py-3 text-base outline-none"
+            />
+            <p className="text-xs text-red-400">{errors.nome?.message}</p>
+          </div>
 
-      <div className="flex flex-col gap-2">
-        <label
-          htmlFor="descricao"
-          className="text-muted font-sans text-sm tracking-wider"
-        >
-          Descrição
-        </label>
-        <textarea
-          id="descricao"
-          {...register("descricao")}
-          placeholder="A descrição da sua carta"
-          className="bg-background focus:border-primary max-h-50 min-h-30 rounded-lg border border-white/10 px-3 py-3 font-sans text-base outline-none"
-        />
-        <p className="text-xs text-red-400">{errors.descricao?.message}</p>
-      </div>
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="descricao"
+              className="text-muted font-belwe text-sm tracking-wider"
+            >
+              Descrição
+            </label>
+            <textarea
+              id="descricao"
+              {...register("descricao")}
+              placeholder="Protege a natureza e seus aliados com força ancestral."
+              className="bg-background/40 focus:border-primary font-belwe max-h-50 min-h-30 rounded-lg border border-white/10 px-3 py-3 text-base outline-none"
+            />
+            <p className="text-xs text-red-400">{errors.descricao?.message}</p>
+          </div>
 
-      <div className="flex gap-3">
-        <div className="flex w-1/2 flex-col gap-2">
-          <label className="text-muted font-sans text-sm tracking-wider">
-            Ataque
-          </label>
-          <input
-            type="number"
-            {...register("ataque", { valueAsNumber: true })}
-            className="bg-background focus:border-primary rounded-lg border border-white/10 px-3 py-3 font-sans text-base outline-none"
-          />
-          <p className="text-xs text-red-400">{errors.ataque?.message}</p>
-        </div>
+          <div className="flex gap-3">
+            <div className="flex w-1/2 flex-col gap-2">
+              <label className="text-muted font-belwe text-sm tracking-wider">
+                Ataque
+              </label>
+              <input
+                type="number"
+                {...register("ataque", { valueAsNumber: true })}
+                className="bg-background/40 focus:border-primary font-belwe rounded-lg border border-white/10 px-3 py-3 text-base outline-none"
+              />
+              <p className="text-xs text-red-400">{errors.ataque?.message}</p>
+            </div>
 
-        <div className="flex w-1/2 flex-col gap-2">
-          <label className="text-muted tracking-widre font-sans text-sm">
-            Defesa
-          </label>
-          <input
-            type="number"
-            {...register("defesa", { valueAsNumber: true })}
-            className="bg-background focus:border-primary rounded-lg border border-white/10 px-3 py-3 font-sans text-base outline-none"
-          />
-          <p className="text-xs text-red-400">{errors.defesa?.message}</p>
-        </div>
-      </div>
+            <div className="flex w-1/2 flex-col gap-2">
+              <label className="text-muted tracking-widre font-belwe text-sm">
+                Defesa
+              </label>
+              <input
+                type="number"
+                {...register("defesa", { valueAsNumber: true })}
+                className="bg-background/40 focus:border-primary font-belwe rounded-lg border border-white/10 px-3 py-3 text-base outline-none"
+              />
+              <p className="text-xs text-red-400">{errors.defesa?.message}</p>
+            </div>
+          </div>
 
-      <div className="flex flex-col gap-2">
-        <label className="text-muted tracking-widre font-sans text-sm">
-          Tipo
-        </label>
-        <select
-          {...register("tipo")}
-          className={`bg-background focus:border-primary rounded-lg border border-white/10 px-3 py-3 font-sans text-base outline-none ${!tipo ? "text-muted" : "text-base"} `}
-        >
-          <option value="">Selecione seu tipo</option>
-          {Object.values(CardType).map((tipo) => (
-            <option key={tipo} value={tipo} className="text-base">
-              {tipo}
-            </option>
-          ))}
-        </select>
-        <p className="text-xs text-red-400">{errors.tipo?.message}</p>
-      </div>
+          <div className="flex flex-col gap-2">
+            <label className="text-muted tracking-widre font-belwe text-sm">
+              Tipo
+            </label>
+            <select
+              {...register("tipo")}
+              className={`bg-background/40 focus:border-primary font-belwe rounded-lg border border-white/10 px-3 py-3 text-base outline-none ${!tipo ? "text-muted" : "text-base"} `}
+            >
+              <option className="bg-surface" value="">
+                Selecione seu tipo
+              </option>
+              {Object.values(CardType).map((tipo) => (
+                <option
+                  key={tipo}
+                  value={tipo}
+                  className="bg-surface text-base"
+                >
+                  {tipo}
+                </option>
+              ))}
+            </select>
+            <p className="text-xs text-red-400">{errors.tipo?.message}</p>
+          </div>
 
-      <div className="flex flex-col gap-2">
-        <label className="text-muted tracking-widre font-sans text-sm">
-          Classe
-        </label>
-        <select
-          {...register("classe")}
-          className={`bg-background focus:border-primary rounded-lg border border-white/10 px-3 py-3 font-sans text-base outline-none ${!classe ? "text-muted" : "text-base"} `}
-        >
-          <option value="">Selecione sua classe</option>
-          {Object.values(CardClass).map((classe) => (
-            <option key={classe} value={classe} className="text-base">
-              {classe}
-            </option>
-          ))}
-        </select>
-        <p className="text-xs text-red-400">{errors.classe?.message}</p>
-      </div>
+          <div className="flex flex-col gap-2">
+            <label className="text-muted tracking-widre font-belwe text-sm">
+              Classe
+            </label>
+            <select
+              {...register("classe")}
+              className={`bg-background/40 focus:border-primary font-belwe rounded-lg border border-white/10 px-3 py-3 text-base outline-none ${!classe ? "text-muted" : "text-base"} `}
+            >
+              <option className="bg-surface" value="">
+                Selecione sua classe
+              </option>
+              {Object.values(CardClass).map((classe) => (
+                <option
+                  key={classe}
+                  value={classe}
+                  className="bg-surface text-base"
+                >
+                  {classe}
+                </option>
+              ))}
+            </select>
+            <p className="text-xs text-red-400">{errors.classe?.message}</p>
+          </div>
 
-      <button
-        type="submit"
-        disabled={!isValid || isSubmitting}
-        className={`bg-primary text-contrast disabled:bg-primary-soft/50 hover:bg-primary-soft mt-2 rounded-md py-2 font-semibold transition ${isSubmitting ? "cursor-progress" : "cursor-pointer disabled:cursor-not-allowed"}`}
-      >
-        {isSubmitting ? "Salvando carta..." : "Salvar carta"}
-      </button>
-    </form>
+          <div className="mt-2 flex items-center justify-center gap-4 self-end">
+            <DialogClose className="font-belwe text-muted h-full cursor-pointer px-4 py-2 hover:text-base">
+              Cancelar
+            </DialogClose>
+            <button
+              type="submit"
+              disabled={!isValid || isSubmitting}
+              className={`bg-primary font-belwe text-contrast disabled:bg-primary-soft/50 hover:bg-primary-soft h-full rounded-md px-4 py-2 font-semibold transition ${isSubmitting ? "cursor-progress" : "cursor-pointer disabled:cursor-not-allowed"}`}
+            >
+              {isSubmitting ? "Salvando carta..." : "Salvar carta"}
+            </button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
   );
 }
---
+++ src/components/CreateCard.tsx
@@ -0,0 +1,26 @@
+import { motion } from "motion/react";
+import { mockCards } from "../mock/mockCards";
+import { Plus } from "lucide-react";
+
+interface CreateCardProps {
+  handleOpenModal: (isOpen: boolean) => void;
+}
+export default function CreateCard({ handleOpenModal }: CreateCardProps) {
+  return (
+    <motion.button
+      onClick={() => handleOpenModal(true)}
+      disabled={mockCards.length === 30}
+      whileHover={{ scale: 0.99 }}
+      className="border-muted/70 relative grid h-80 w-55 cursor-pointer flex-col place-items-center gap-3 rounded-2xl border border-dashed bg-linear-to-b from-stone-800 to-stone-900 p-2 shadow-2xl disabled:cursor-not-allowed disabled:opacity-50"
+    >
+      <div className="flex flex-col items-center gap-5">
+        <div className="border-muted/70 flex size-16 items-center justify-center rounded-full border border-dashed">
+          <Plus size={30} className="text-muted/50" />
+        </div>
+        <span className="text-muted/50 font-sans font-semibold tracking-wide">
+          NOVA CARTA
+        </span>
+      </div>
+    </motion.button>
+  );
+}
