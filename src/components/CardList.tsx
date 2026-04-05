91bf2b92dbaec4269133a9514569bf6a8c6d7e0a
Luis Soares <luissoaress.dev@gmail.com>
2026-04-05 11:08:59 -0300

    feat: add floating action button for mobile to open card form

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import CardItem from "./CardItem";
 import CreateCard from "./CreateCard";
 import CardForm from "./CardForm";
 import { useCards } from "../context/CardContext";
+import { Plus } from "lucide-react";
+import { motion, AnimatePresence, useInView } from "motion/react";
 
 export default function CardList() {
-  const { filteredCards } = useCards();
   const [openModal, setOpen] = useState(false);
+  const { filteredCards } = useCards();
+  const refCreate = useRef(null);
+  const isInView = useInView(refCreate);
+  const showButton = !isInView;
+
   const handleOpenModal = (isOpen: boolean) => setOpen(isOpen);
 
   return (
@@ -16,10 +22,18 @@ export default function CardList() {
         <h3 className="font-belwe text-primary text-center text-lg md:text-xl">
           MINHAS CARTAS
         </h3>
+
         <div
-          className={`grid w-full place-items-center gap-8 px-2 md:gap-10 ${filteredCards.length === 0 ? "col-span-1" : "min-[550px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"}`}
+          className={`grid w-full place-items-center gap-8 px-2 md:gap-10 ${
+            filteredCards.length === 0
+              ? "col-span-1"
+              : "min-[34.375rem]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
+          }`}
         >
-          <CreateCard handleOpenModal={handleOpenModal} />
+          <div ref={refCreate}>
+            <CreateCard handleOpenModal={handleOpenModal} />
+          </div>
+
           {filteredCards.map((card) => (
             <CardItem
               key={card.id}
@@ -28,6 +42,21 @@ export default function CardList() {
             />
           ))}
         </div>
+
+        <AnimatePresence>
+          {showButton && (
+            <motion.button
+              onClick={() => handleOpenModal(true)}
+              initial={{ opacity: 0, scale: 0.6 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.6 }}
+              transition={{ duration: 0.25 }}
+              className="border-primary/50 fixed right-10 bottom-12 z-50 grid size-12 place-items-center rounded-full border active:scale-95 lg:hidden"
+            >
+              <Plus size={26} className="text-primary/50" />
+            </motion.button>
+          )}
+        </AnimatePresence>
       </section>
     </>
   );
